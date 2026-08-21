a59a2bfcf31ad26948013b1f88acb3d47287d046
MaxiGP0 <maximogomezpessoa@gmail.com>
2026-08-21 00:39:32 -0300

    probando la impresion

--- Games/batalla-naval/main.c
+++ Games/batalla-naval/main.c
@@ -2,25 +2,32 @@
 
 
 #include <stdio.h> 
-#include <stdlib.h>
+// #include <stdlib.h>
 #include "funciones.h" 
+
+
+
+#define FILAS 10
+#define COLUMNAS 10
 
 
 int main(void) {
 
 
-    int** mapa = crear_mapa(10, 10);
+    int** Player1 = crear_mapa(FILAS, COLUMNAS);
+    int** Player2 = crear_mapa(FILAS, COLUMNAS);
 
-    imprimir_mapa(10, 10, mapa);
-
-    printf("\n");
+    imprimir_mapa(FILAS, COLUMNAS, Player1);
+    printf("-------------------\n");
+    imprimir_mapa(FILAS, COLUMNAS, Player2);
     
 
 
 
 
 
-    free(mapa);
+    liberar_mapa(FILAS, Player1);
+    liberar_mapa(FILAS, Player2);
     return 0;
 
 }
